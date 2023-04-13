e6eb38451b30bdebb65041d1deab456e7bf9a4b2
cc0803 <simongeldbach@gmail.com>
2023-04-13 18:14:25 +0200

    Solve unclickable link at high speeds problem

--- GameOne/app.js
+++ GameOne/app.js
@@ -6,16 +6,15 @@ startButton.addEventListener("click", () => {
     event.preventDefault();
     overlay.classList.add("hidden");
     chooseStarfighter();
-})
+});
 
 function chooseStarfighter() {
     const selection = document.querySelector("input:checked").value;
-    let starfighter;
     let image = document.querySelector(".starship>img");
+
     if (selection == "tie") {
-        image.src = "../Images/Tie-Fighter.png";
+        image.src = "../Images/TIE-Fighter.png";
         image.alt = "Bild eines TIE-Fighters";
-        starfighter = document.querySelector(".starship");
         fraction = "empire";
         
     } else {
